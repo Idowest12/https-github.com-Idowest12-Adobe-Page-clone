b2304c7d9f2de24faf449248da30f253fc17dd4d
Tosin <idowutosin70@gmail.com>
2023-09-19 09:28:10 -0700

    Original

---
+++ images/Adobe.js
@@ -0,0 +1,7 @@
+// let btnSearch = document.getElementById('search-icon');
+// let searchInput = document.getElementById('search');
+
+// btnSearch.addEventListener ('click', ()=>{
+//     searchInput.style.display = 'block';
+//     body.style.backgroundColor = "blue";
+// })    
